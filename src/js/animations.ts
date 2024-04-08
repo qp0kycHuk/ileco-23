148babe787f9d890f2f0424e769ac67fa1755484
qp0kycHuk <serega990112@mail.ru>
2024-04-08 11:16:36 +0300

    add animation

--- src/js/animations.ts
+++ src/js/animations.ts
@@ -1,8 +1,37 @@
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
+const gsapAnimation: Record<string, any> = {
+  fadeInLeft: {
+    from: { x: '-75', opacity: 0 },
+    to: { x: 0, opacity: 1 },
+  },
+  fadeInRight: {
+    from: { x: '75', opacity: 0 },
+    to: { x: 0, opacity: 1 },
+  },
+  fadeInUp: {
+    from: { y: '75', opacity: 0 },
+    to: { y: 0, opacity: 1 },
+  },
+  fadeInDown: {
+    from: { y: '-75', opacity: 0 },
+    to: { y: 0, opacity: 1 },
+  },
+  fadeIn: {
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+  },
+  scaleIn: {
+    from: { scale: 1.5 },
+    to: { scale: 1 },
+  },
+}
+
 function init() {
   gsap.registerPlugin(ScrollTrigger)
+
+  scrollInit()
 
   // parallax
   {
@@ -24,6 +53,48 @@ function init() {
       )
     })
   }
+}
+
+function scrollInit() {
+  const scrollElements = document.querySelectorAll('[data-animation]')
+
+  scrollElements.forEach((element) => {
+    const key = element.getAttribute('data-animation')
+    const duration = element.getAttribute('data-duration') || 1
+    const end = element.getAttribute('data-end') || 'bottom 40px'
+    const start = element.getAttribute('data-start') || 'top 100%'
+    const delay = element.getAttribute('data-delay') || 0.1
+    const actions = element.getAttribute('data-actions') || 'play reverse play reverse'
+
+    const animation = gsapAnimation[key || '']
+
+    if (!animation) {
+      return
+    }
+
+    const instance = gsap.fromTo(element, animation.from, {
+      ...animation.to,
+      duration,
+      delay,
+      ease: 'elastic.out(1,1)',
+      scrollTrigger: {
+        trigger: element,
+        start: start,
+        end: end,
+        toggleActions: actions,
+
+        // scrub: true,
+        // markers: true,
+        // id: "scrub"
+      },
+    })
+
+    instance.then(() => {
+      gsap.set(element, {
+        transform: 'none',
+      })
+    })
+  })
 }
 
 export default { init }
