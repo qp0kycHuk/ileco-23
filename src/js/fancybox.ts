906a2bf5ea49392d3d73abd7f01853389b7293f4
qp0kycHuk <serega990112@mail.ru>
2024-05-29 11:27:43 +0300

    add dialog

--- src/js/fancybox.ts
+++ src/js/fancybox.ts
@@ -20,6 +20,9 @@ function init() {
     // @ts-ignore
     Fancybox.show([{ src, ...options }], options)
   }
+
+  // @ts-ignore
+  // Fancybox.modal.open('dialog-review.html')
 }
 
 interface CustomWindow extends Window {
